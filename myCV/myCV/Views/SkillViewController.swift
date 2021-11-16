72a3c8a5ae41a1cf95d41a9e3ec7b3e61c56fd3a
Aleksander Holod <xolod2002@yandex.ru>
2021-11-16 23:11:25 +0300

    Добавил final в SkillViewController. Был создан enum куда были перенесены все ограничения. Были добавлены модификаторы доступа.

--- myCV/myCV/Views/SkillViewController.swift
+++ myCV/myCV/Views/SkillViewController.swift
@@ -7,51 +7,76 @@
 
 import UIKit
 
-class SkillViewController: UIViewController {
+final class SkillViewController: UIViewController {
     
-    let skillsLabel:UILabel = {
+    private enum ConstantSkill {
+        static let skillLabelSize: CGFloat = 25
+        static let skillLabelTopAnchor: CGFloat = 20
+        
+        static let pyLabelSize: CGFloat = 22
+        static let pyLabelLeadingAcnhor: CGFloat = 40
+        static let pyLabelTrailingAnchor: CGFloat = -40
+        static let pyLAbelTopAnchor: CGFloat = 40
+        
+        static let pySkillSize: CGFloat = 16
+        static let pySkillLeadingAnchor: CGFloat = 40
+        static let pySkillTrailingAnchor: CGFloat = -40
+        static let pySkillTopAnchor: CGFloat = 20
+        
+        static let swiftLabelSize: CGFloat = 22
+        static let swiftLabelLeadingAnchor: CGFloat = 40
+        static let swiftLabelTrailingAnchor: CGFloat = -40
+        static let swiftLabelTopAnchor: CGFloat = 30
+        
+        static let swiftSkillSize: CGFloat = 16
+        static let swiftSkillLeadingAnchor: CGFloat = 40
+        static let swiftSkillTrailingAnchor: CGFloat = -40
+        static let swiftSkillTopAnchor: CGFloat = 20
+    }
+    
+    private let skillsLabel:UILabel = {
         let skillLabel = UILabel()
         skillLabel.text = "Мои навыки 📚"
-        skillLabel.font = UIFont.boldSystemFont(ofSize: 25)
+        skillLabel.font = UIFont.boldSystemFont(ofSize: ConstantSkill.skillLabelSize)
         skillLabel.translatesAutoresizingMaskIntoConstraints = false
         
         return skillLabel
     }()
     
-    let pyLabel: UILabel = {
+    private let pyLabel: UILabel = {
         let pyLabel = UILabel()
         pyLabel.text = "Python"
-        pyLabel.font = .boldSystemFont(ofSize: 22)
+        pyLabel.font = .boldSystemFont(ofSize: ConstantSkill.pyLabelSize)
         pyLabel.translatesAutoresizingMaskIntoConstraints = false
         
         return pyLabel
     }()
     
-    let pySkill: UILabel = {
+    private let pySkill: UILabel = {
         let pySkill = UILabel()
         pySkill.text = "\tВ рамках учебы решал различные базовые задачи плюс работал с распознавание объектов с помощью библиотек OpenCV, ImageAI и анализом речии Vosk."
         pySkill.numberOfLines = 0
-        pySkill.font = UIFont.systemFont(ofSize: 16)
+        pySkill.font = UIFont.systemFont(ofSize: ConstantSkill.pySkillSize)
         pySkill.textAlignment = .justified
         pySkill.translatesAutoresizingMaskIntoConstraints = false
         return pySkill
     }()
         
     
-    let swiftLabel: UILabel = {
+    private let swiftLabel: UILabel = {
         let swiftLabel = UILabel()
         swiftLabel.text = "Swift"
-        swiftLabel.font = .boldSystemFont(ofSize: 22)
+        swiftLabel.font = .boldSystemFont(ofSize: ConstantSkill.swiftLabelSize)
         swiftLabel.translatesAutoresizingMaskIntoConstraints = false
         
         return swiftLabel
     }()
     
-    let swiftSkill: UILabel = {
+    private let swiftSkill: UILabel = {
         let swiftSkill = UILabel()
         swiftSkill.text = "\tSwift я изучаю недавно и каждый день я по нему узнаю много нового. Например, сегодня первый раз верстаю кодом)). Параллельно смотрю ролики на Swiftbook и пытаюсь улучшать свои навыки"
         swiftSkill.numberOfLines = 0
-        swiftSkill.font = UIFont.systemFont(ofSize: 16)
+        swiftSkill.font = UIFont.systemFont(ofSize: ConstantSkill.swiftSkillSize)
         swiftSkill.textAlignment = .justified
         swiftSkill.translatesAutoresizingMaskIntoConstraints = false
         
@@ -89,34 +114,34 @@ class SkillViewController: UIViewController {
         
     }
     
-    func setupSkillsLabel() {
+    private func setupSkillsLabel() {
         skillsLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        skillsLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        skillsLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: ConstantSkill.skillLabelTopAnchor).isActive = true
     }
     
-    func setupPyLabel() {
-        pyLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
-        pyLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
-        pyLabel.topAnchor.constraint(equalTo: skillsLabel.bottomAnchor, constant: 40).isActive = true
+    private func setupPyLabel() {
+        pyLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: ConstantSkill.pyLabelLeadingAcnhor).isActive = true
+        pyLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: ConstantSkill.pyLabelTrailingAnchor).isActive = true
+        pyLabel.topAnchor.constraint(equalTo: skillsLabel.bottomAnchor, constant: ConstantSkill.pyLAbelTopAnchor).isActive = true
         
     }
     
-    func setupPySkill() {
-        pySkill.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
-        pySkill.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
-        pySkill.topAnchor.constraint(equalTo: pyLabel.bottomAnchor, constant: 20).isActive = true
+    private func setupPySkill() {
+        pySkill.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: ConstantSkill.pySkillLeadingAnchor).isActive = true
+        pySkill.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: ConstantSkill.pySkillTrailingAnchor).isActive = true
+        pySkill.topAnchor.constraint(equalTo: pyLabel.bottomAnchor, constant: ConstantSkill.pySkillTopAnchor).isActive = true
     }
     
-    func setupSwiftLabel() {
-        swiftLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
-        swiftLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
-        swiftLabel.topAnchor.constraint(equalTo: pySkill.bottomAnchor, constant: 30).isActive = true
+    private func setupSwiftLabel() {
+        swiftLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: ConstantSkill.swiftLabelLeadingAnchor).isActive = true
+        swiftLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: ConstantSkill.swiftLabelTrailingAnchor).isActive = true
+        swiftLabel.topAnchor.constraint(equalTo: pySkill.bottomAnchor, constant: ConstantSkill.swiftLabelTopAnchor).isActive = true
     }
     
-    func setupSwiftSkill() {
-        swiftSkill.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
-        swiftSkill.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
-        swiftSkill.topAnchor.constraint(equalTo: swiftLabel.bottomAnchor, constant: 20).isActive = true
+    private func setupSwiftSkill() {
+        swiftSkill.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: ConstantSkill.swiftSkillLeadingAnchor).isActive = true
+        swiftSkill.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: ConstantSkill.swiftSkillTrailingAnchor).isActive = true
+        swiftSkill.topAnchor.constraint(equalTo: swiftLabel.bottomAnchor, constant: ConstantSkill.swiftSkillTopAnchor).isActive = true
     }
     
 }
