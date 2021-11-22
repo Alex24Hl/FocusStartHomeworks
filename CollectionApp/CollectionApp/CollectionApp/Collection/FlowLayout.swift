94d85588d4ea833441d7709867f7ccd65d4f4b51
Aleksander Holod <xolod2002@yandex.ru>
2021-11-22 23:10:31 +0300

    Добавлена папка Collection

---
+++ CollectionApp/CollectionApp/CollectionApp/Collection/FlowLayout.swift
@@ -0,0 +1,38 @@
+//
+//  FlowLayout.swift
+//  CollectionApp
+//
+//  Created by Александр Холод on 21.11.2021.
+//
+
+import UIKit
+
+final class FlowLayout: UICollectionViewFlowLayout {
+    
+    enum Constant {
+        static let portrait = 2
+        static let land = 5
+    }
+
+    var sectionInserts = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        
+    override func prepare() {
+        super.prepare()
+    
+        guard let collectionView = self.collectionView else { return }
+        
+        let screen = UIScreen.main.bounds
+        let itemsPerRows = (screen.height > screen.width) ? Constant.portrait : Constant.land
+        var availableWidth = collectionView.bounds.inset(by: collectionView.layoutMargins).size.width
+        availableWidth -= CGFloat(itemsPerRows + 1) * self.minimumLineSpacing
+        
+        let cellWidth = (availableWidth / CGFloat(itemsPerRows)).rounded(.down)
+        
+        self.itemSize = CGSize(width: cellWidth, height: cellWidth)
+        self.sectionInserts = UIEdgeInsets(top: self.minimumLineSpacing * 2, left: self.minimumInteritemSpacing, bottom: 0, right: self.minimumInteritemSpacing)
+        self.sectionInsetReference = .fromLayoutMargins
+    }
+}
+
+   
+    
